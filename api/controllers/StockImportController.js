abed0782d36a1e6c24107ec5fefe8b8575f014b1
Steven van der Merwe <stevenvdmerwe@gmail.com>
2017-05-07 18:13:57 +0200

    Added functionality to upload and convert to json docs

--- api/controllers/StockImportController.js
+++ api/controllers/StockImportController.js
@@ -5,9 +5,33 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-var xlsxj = require("xlsx-to-json");
+var fs = require('fs');
+var xlsxj = require("xlsx-to-json-lc");
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('temp-db.sqlite');
 
 module.exports = {
+    parseJson: function(req,res) {
+        var jsonStringArray = fs.readFile("stock.json", function(err, data) {
+            if(err) {
+                res.serverError(err);
+            }
+            var stockArray = JSON.parse(jsonStringArray);
+
+            var messages = {
+                data: []
+            };
+
+            stockArray.forEach(function(element) {
+                // console.log(element);
+                messages.data.push(element);
+            }, this);
+            
+            res.json({
+                message: messages
+            });
+        }); 
+    },
 	upload: function(req, res) {
         req.file('stockFile').upload(function(err, files) {
             if(err) {
@@ -23,9 +47,36 @@ module.exports = {
                 output: "stock.json"
             }, function(err, result) {
                 if(err) {
-                console.error(err);
+                    res.serverError(err);
                 }else {
-                console.log(result);
+                    // console.log(result);
+                    // try {
+                    //     var jsonStringArray = fs.readFile("stock.json", function(err, data) {
+                    //         if(err) {
+                    //             res.serverError(err);
+                    //         }
+                    //         var stockArray = JSON.parse(jsonStringArray);
+
+                    //         db.serialize(function() {
+                    //             db.run("CREATE TABLE IF NOT EXISTS \"StockItem\"( " +
+                    //                 "\StockCode\" varchar ," +
+                    //                 "\"BarCode\" varchar ," +
+                    //                 "\"StockDescription\" varchar ," +
+                    //                 "\"StockQuantity\" integer ," +
+                    //                 "\"StockPrice\" varchar ," +
+                    //                 "\"DateUpdated\" varchar ," +
+                    //                 "\"DateUpdatedActual\" datetime );");
+
+                    //             stockArray.forEach(function(element) {
+                    //                 console.log(element);
+                    //             }, this);
+                    //         }); 
+                    //         db.close();    
+                    //     });                          
+                    // }
+                    // catch (err) {
+                    //     db.close(); 
+                    // }
                 }
             });
 
